98e92636371662f54c32e093f2f1538d2172ca28
Brian Gossett <brian.gossett.usa@gmail.com>
2026-07-18 00:03:56 -0500

    Add LogManager autoload
    Closes #4

---
+++ autoloads/log_manager.gd
@@ -0,0 +1,15 @@
+extends Node
+
+signal line_added(timestamped_text: String)
+
+const MAX_LINES: int = 200
+
+var _lines: Array[String] = []
+
+
+func push(text: String) -> void:
+	var line := "[%s] %s" % [Time.get_time_string_from_system(), text.to_lower()]
+	_lines.append(line)
+	if _lines.size() > MAX_LINES:
+		_lines.pop_front()
+	line_added.emit(line)
